86610da15b270e6e40ef1da8ff90c2e65fc92095
Oleg Fostij <fostijoleg@gmail.com>
2017-01-22 15:10:36 +0200

    Initial commit

---
+++ switch.php
@@ -0,0 +1,22 @@
+<?php
+$oplata = "Pochta";
+switch($oplata){
+    case "Bank":
+    echo "Ви вибрали способ оплати банк...";
+    break;
+
+    case "Karta":
+    echo "Ви вибрали способ оплати через карту...";
+    break;
+
+    case "Pochta":
+    echo "Ви вибрали способ оплати через почту...";
+    break;
+
+    default:
+    echo "Ви нічого не вибрали";
+    break;
+
+}
+
+?>
---
+++ var.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Олег \"Фостій\"";
+$age = 27;
+$a = 23;
+$summa = $a + $age;
+
+echo "<strong>Мене звати</strong> - $name мені $age через 23 роки мені буде $summa<br>";
+$f = $age - $a;
+$b = $age * $a;
+$c = $age / $a;
+echo "<br>$f<br>";
+echo " <br>$b<br>";
+echo "<br>$c<br>";
+$d = $age.$a;
+echo "<br>$d<br>";
+
+?>
